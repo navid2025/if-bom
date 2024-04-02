085eb40aab49488d260401c33c8db1fe6adf975d
n.zare <n.zare@behsacorp.com>
2024-04-02 17:04:28 +0330

    initial commit

---
+++ src/main/java/com/behsa/ifbom/IfBomApplication.java
@@ -0,0 +1,13 @@
+package com.behsa.ifbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfBomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/behsa/ifbom/IfBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behsa.ifbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
